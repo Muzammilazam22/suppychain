546b2c388c9ddcf6115bf10426203301e07a397b
muzammil <muzammil.azam88@gmail.com>
2021-05-19 12:59:15 +0500

    Contract Deployed Sucessfully

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,6 @@
+const Migrations = artifacts.require("Migrations");
+const ItemManager = artifacts.require("ItemManager");
+module.exports = function (deployer) {
+  deployer.deploy(Migrations);
+  deployer.deploy(ItemManager);
+};
